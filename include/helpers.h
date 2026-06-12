4146c7ba9e284b1a849785085e9b50b5a60db2d9
gevorgKhachikyan <gev.khachikyan07@gmail.com>
2026-06-12 20:49:57 +0400

    Remove unused fin variable from helpers.h

--- include/helpers.h
+++ include/helpers.h
@@ -16,7 +16,6 @@ struct History
 
 extern std::array<std::array<char,10>,10> grid;
 extern std::array<std::array<int,10>,10> gridCount;
-extern std::ifstream fin;
 extern std::array<int,2> lastWordIndexes;
 extern bool wasHorizontal, wasVertical;
 extern int wLen;
